43fb15563cc2bd3ca4dcea18ffa6c15a75da60c3
KseniyaV <kseniyaverenich@gmail.com>
2019-03-25 12:09:04 -0400

    Added temp. nav component

--- roadrunner/src/App.vue
+++ roadrunner/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+
+<NavBar>
+</NavBar>
+
+
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,11 +12,11 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from './components/NavBar.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld, NavBar
   }
 }
 </script>
