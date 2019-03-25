1cef5ef13b1bda40bfbe51e162cbe1a09ac9c6bb
Colby <colbytaylor1212@gmail.com>
2019-03-25 11:50:30 -0400

    added basic data structure

--- roadrunner/src/App.vue
+++ roadrunner/src/App.vue
@@ -10,6 +10,43 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data: { cars: [
+        {
+          make: 'dodge', 
+          model: 'viper',
+          color: 'red',
+          rented: false,
+          img: 'image location',
+          location: '123 awesome inc way'
+        },
+        {
+          make: 'chevy', 
+          model: 'corvette',
+          color: 'blue',
+          rented: true,
+          img: 'image location',
+          location: '456 lexington road'},
+          ],
+          
+          users: [
+          {
+          name: 'name',
+          email: 'email address',
+          age: 34,
+          phone: '895-555-5555',
+          password: 'password',
+          senior: false,
+          },
+          {
+          name: 'name',
+          email: 'email address',
+          age: 66,
+          phone: '895-555-5555',
+          password: 'password',
+          senior: true,
+          },
+         ],
+  
   components: {
     HelloWorld
   }
