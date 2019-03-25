8447fc5574b17cdc6093fc892fa27e943e5b6623
KseniyaV <kseniyaverenich@gmail.com>
2019-03-25 16:49:44 -0400

    added about me text and HowItWorks icons

--- roadrunner/src/components/AboutUs.vue
+++ roadrunner/src/components/AboutUs.vue
@@ -1,17 +1,48 @@
 <template>
-  <div class="jumbotron jumbotron-fluid">
+  <div class="jumbotron jumbotron-fluid" id="AboutUs">
     <div class="container">
       <div class="row">
         <div class="col">
+          <center>
+            <h1>About Us</h1>
+          </center>
+          <p>Road-Runner Exotic Car Rental allows you to experience a thrill that will last long after the rental return.</p>
+          <p>Specializing in exotic, rare, and and exclusive cars, we provide a service unmatched by the car industry.</p>
 
-        <h1>About Us</h1>
-      </div>
-        <div class="col">
-
-        <h1>About Us</h1>
+          <br>
+          <br>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h1>How It Works</h1>
+            </div>
+          </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h1>
+                <i class="material-icons">filter_1</i>
+              </h1>
+            </div>
+            <div class="col">
+              <h1>
+                <i class="material-icons">filter_2</i>
+              </h1>
+            </div>
+            <div class="col">
+              <h1>
+                <i class="material-icons">filter_3</i>
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
