1edb5abef025c5db24bf984b4a048ec19c2e40cb
Colby <colbytaylor1212@gmail.com>
2019-03-26 11:53:26 -0400

    updated file strucutre

--- roadrunner/src/main.js
+++ roadrunner/src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
-import Vuex from 'vuex'
+
 
 Vue.use(BootstrapVue)
 
-Vue.use(Vuex)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
