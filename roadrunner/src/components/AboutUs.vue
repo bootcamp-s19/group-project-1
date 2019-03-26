f9e8daba3ad148a97f9296c86f2a0a41c82e600b
KseniyaV <kseniyaverenich@gmail.com>
2019-03-26 11:51:36 -0400

    revised file structure/added Store file

---
+++ roadrunner/src/components/AboutUs.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="jumbotron jumbotron-fluid" id="AboutUs">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <center>
+            <h1>About Us</h1>
+          </center>
+          <p>Road-Runner Exotic Car Rental allows you to experience a thrill that will last long after the rental return.</p>
+          <p>Specializing in exotic, rare, and and exclusive cars, we provide a service unmatched by the car industry.</p>
+
+          <br>
+          <br>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h1>How It Works</h1>
+            </div>
+          </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h1>
+                <i class="material-icons">filter_1</i>
+              </h1>
+            </div>
+            <div class="col">
+              <h1>
+                <i class="material-icons">filter_2</i>
+              </h1>
+            </div>
+            <div class="col">
+              <h1>
+                <i class="material-icons">filter_3</i>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+export default {
+  name: "AboutUs",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: left;
+}
