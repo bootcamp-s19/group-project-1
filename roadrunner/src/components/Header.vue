f48fcffd0631e183d1d143b16dee97cd4b86e2a4
KseniyaV <kseniyaverenich@gmail.com>
2019-03-27 10:09:43 -0400

    Text block made resposive. Status- done.

--- roadrunner/src/components/Header.vue
+++ roadrunner/src/components/Header.vue
@@ -1,30 +1,28 @@
 <template>
-
-
   <div class="mainContainer">
     <img src="/img/header.jpg">
 
- <div class="text-block"> 
-    <h1>ROAD-RUNNER</h1>
-    <h3>Exotic Car Rental</h3>
-    <button type="button" class="btn btn-dark">SEARCH</button>
-     <button type="button" class="btn btn-dark">SUBSCRIBE</button>
-     <br><br>
+    <div class="text-block">
+      <h1>ROAD-RUNNER</h1>
+      <h3>Exotic Car Rental</h3>
+      <button type="button" class="btn btn-dark btn-lg">SEARCH</button>
+      <button type="button" class="btn btn-dark btn-lg">SUBSCRIBE</button>
+      <br>
+      <br>
 
-  <!-- <div class="col2">
+      <!-- <div class="col2">
    <h1> ROAD-RUNNER </h1>
      <button class="btn">BROWSE</button>
-     <button class="btn">SUBSCRIBE</button> -->
-
-
-    
-</div>
+      <button class="btn">SUBSCRIBE</button>-->
+    </div>
   </div>
 </template>
 
 <style scoped>
-h1, h3, .btn {
-  font-family: 'Fugaz One', cursive;
+h1,
+h3,
+.btn {
+  font-family: "Fugaz One", cursive;
 }
 .btn {
   background-color: #555;
@@ -34,29 +32,78 @@ h1, h3, .btn {
 
 .col {
   background-color: white;
-
 }
-
 
 .mainContainer {
   position: relative;
   width: 100%;
 }
 .container {
-position: relative;
-
+  position: relative;
 }
 
-.text-block {
-  position: absolute;
-  bottom: 20px;
-  left: 20px;
-  background-color: black;
-  opacity: 0.7;
-  filter: alpha(opacity=70);
-  color: white;
-  padding-left: 20px;
-  padding-right: 20px;
+@media (min-width: 576px) {
+  .text-block {
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+    background-color: black;
+    opacity: 0.7;
+    filter: alpha(opacity=70);
+    color: white;
+    padding-left: 70px;
+    padding-right: 70px;
+    padding-top:20px;
+    padding-bottom:20px;
+  }
+}
+
+@media (min-width: 768px) {
+  .text-block {
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+    background-color:black;
+    opacity: 0.7;
+    filter: alpha(opacity=70);
+    color: white;
+    padding-left: 70px;
+    padding-right: 70px;
+    padding-top:20px;
+    padding-bottom:20px;
+  }
+}
+
+@media (min-width: 992px) {
+  .text-block {
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+    background-color:black;
+    opacity: 0.7;
+    filter: alpha(opacity=70);
+    color: white;
+    padding-left: 70px;
+    padding-right: 70px;
+    padding-top:20px;
+    padding-bottom:20px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .text-block {
+    position: absolute;
+    bottom: 100px;
+    left: 180px;
+    background-color:black;
+    opacity: 0.7;
+    filter: alpha(opacity=70);
+    color: white;
+    padding-left: 70px;
+    padding-right: 70px;
+    padding-top:20px;
+    padding-bottom:20px;
+  }
 }
 
 .mainContainer img {
@@ -64,8 +111,7 @@ position: relative;
   height: auto;
 }
 
-
-.mainContainer .col2  {
+.mainContainer .col2 {
   position: absolute;
   top: 60%;
   left: 30%;
@@ -77,17 +123,11 @@ position: relative;
   padding: 12px 24px;
   border: none;
   cursor: pointer;
- 
 }
-
-
-
-
 
 .mainContainer .btn:hover {
   background-color: black;
 }
-
 </style>
 
 
