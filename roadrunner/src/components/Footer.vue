f0286f0f9866a5a1a51b0d3078fc5430811aecc3
KseniyaV <kseniyaverenich@gmail.com>
2019-03-27 14:56:23 -0400

    added footer and social links

---
+++ roadrunner/src/components/Footer.vue
@@ -0,0 +1,96 @@
+<template>
+
+<div class="jumbotron jumbotron-fluid" id="jumbo5">
+         <div class="container">
+            <div class="row">
+               <div class="container">
+                  <footer class="footer text-center">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-md-4 mb-5 mb-lg-0">
+                              <h4 class="text-uppercase mb-4" id="location">Location</h4>
+                              <p class="lead mb-0" id="location2">3009 Wishbone Ln
+                                 <br>Lexington, KY 40502
+                              </p>
+                           </div>
+                           <div class="col-md-4 mb-5 mb-lg-0">
+                              <h4 class="text-uppercase mb-4" id="web">Around the Web</h4>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item">
+                                    <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                                      <ion-icon name="logo-google"></ion-icon>
+                                       <i class="fab fa-fw fa-facebook-f"></i>
+                                    </a>
+                                 </li>
+                                 <li class="list-inline-item">
+                                    <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                                       <i class="fab fa-fw fa-google-plus-g"></i>
+                                     <ion-icon name="logo-model-s"></ion-icon>
+                                    </a>
+                                 </li>
+                                 <li class="list-inline-item">
+                                    <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                                       <i class="fab fa-fw fa-twitter"></i>
+                                       <ion-icon name="logo-facebook"></ion-icon>
+                                    </a>
+                                 </li>
+                                 <li class="list-inline-item">
+                                    <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                                       <i class="fab fa-fw fa-linkedin-in"></i>
+                                       <ion-icon name="logo-instagram"></ion-icon>
+                                    </a>
+                                 </li>
+                                 <li class="list-inline-item">
+                                    <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                                       <i class="fab fa-fw fa-dribbble"></i>
+                                       <ion-icon name="logo-twitter"></ion-icon>
+                                    </a>
+                                 </li>
+                              </ul>
+                           </div>
+                           <div class="col-md-4">
+                              <h4 class="text-uppercase mb-4" id="links">Links</h4>
+                              <p class="lead mb-0" id="links2"></p>
+                              <ul id="listnobull">
+                                 <li>
+                                    <a href="#" id="about">About Us</a>
+                                 </li>
+                                 <li><a href="#" id="privacy">Privacy Policy</a></li>
+                                 <li><a href="#" id="find"> Find Polling Location</a> </li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </footer>
+                  <br>
+                  <div class="row float-right">
+                     <button type="button" class="btn btn-light"><a href="#top" >Back to Top!</a></button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+
+
+
+
+</template>
+
+
+
+
+
+
+
+
+
+
+<script>
+export default {
+  name: "Footer",
+  
+  
+};
+</script>
