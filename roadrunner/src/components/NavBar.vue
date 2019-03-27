23bb1e4e703d17c6c218ca0b1b6cdf6ffc6ae0be
KseniyaV <kseniyaverenich@gmail.com>
2019-03-27 13:57:37 -0400

    Changed nav bar format, fixed collapse in mobile

--- roadrunner/src/components/NavBar.vue
+++ roadrunner/src/components/NavBar.vue
@@ -1,10 +1,56 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <b-navbar toggleable="lg" type="dark" variant="info">
       <a class="navbar-brand" href="#">
         <img src="/img/RoadRunner.png">
-        
       </a>
+
+     
+
+      <b-navbar-toggle target="nav_collapse"/>
+
+ <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-navbar-brand href="#">Home</b-navbar-brand>
+        </b-navbar-nav>
+
+
+    
+        <b-navbar-nav>
+          <b-navbar-brand href="#AboutUs">About Us</b-navbar-brand>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-nav>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                <button id="show-modal" @click="showModal = true">Subscribe</button>
+                <Subscribe v-if="showModal" @close="showModal = false"></Subscribe>
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+          </b-navbar-nav>
+          <b-navbar-nav>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                <button id="show-modal" @click="showLoginModal = true">Login</button>
+                <Login v-if="showLoginModal" @close="showLoginModal = false"></Login>
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+          </b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+
+  <!-- <div>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light navbar-collapse">
+      <a class="navbar-brand" href="#">
+        <img src="/img/RoadRunner.png">
+      </a>
+
       <button
         class="navbar-toggler d-lg-none"
         type="button"
@@ -16,6 +62,8 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
+
+
       <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item active">
@@ -32,24 +80,24 @@
           </li>
           <li class="nav-item active">
             <a class="nav-link" href="#">
-                          <button id="show-modal" @click="showModal = true">Subscribe</button>
-            <Subscribe v-if="showModal" @close="showModal = false"></Subscribe>
+              <button id="show-modal" @click="showModal = true">Subscribe</button>
+              <Subscribe v-if="showModal" @close="showModal = false"></Subscribe>
               <span class="sr-only">(current)</span>
             </a>
-
           </li>
           <li class="nav-item active">
             <a class="nav-link" href="#">
-                <button id="show-modal" @click="showLoginModal = true">Login</button>
-                <Login v-if="showLoginModal" @close="showLoginModal = false"></Login>
+              <button id="show-modal" @click="showLoginModal = true">Login</button>
+              <Login v-if="showLoginModal" @close="showLoginModal = false"></Login>
               <span class="sr-only">(current)</span>
             </a>
           </li>
         </ul>
       </div>
     </nav>
-  </div>
+  </div>-->
 </template>
+
 
 <script>
 import Subscribe from "./Subscribe";
@@ -58,7 +106,7 @@ import Login from "./Login";
 export default {
   name: "NavBar",
   components: {
-    Subscribe, 
+    Subscribe,
     Login
   },
   props: {
@@ -67,7 +115,7 @@ export default {
   data() {
     return {
       showModal: false,
-      showLoginModal: false      
+      showLoginModal: false
     };
   }
 };
